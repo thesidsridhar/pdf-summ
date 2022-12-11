4cde489ab98289125638abab0a0b73b78bdb449e
Siddarth Sridhar <70396053+thesidsridhar@users.noreply.github.com>
2022-12-11 14:39:50 -0800

    Update app.js

--- app.js
+++ app.js
@@ -23,40 +23,38 @@ app.get("/", (req, res) => {
 });
 
 app.post("/upload", (req, res) => {
-  // Parse PDF file
-  pdf(req.files.pdf.data).then((data) => {
-    // Prompt user for summary length
-    prompt([
-      {
-        type: "input",
-        name: "length",
-        message: "Enter the desired length of the summary (in words):",
-      },
-    ]).then((answers) => {
-      // Use OpenAI's GPT-3 to generate summary
-      openai.completions
-        .create({
-          prompt: data.text,
-          max_tokens: parseInt(answers.length),
-          temperature: 0.5,
-        })
-        .then((response) => {
-          // Display summary
-          res.send(`
-            <h1>PDF Summary</h1>
-            <p>${response.data.choices[0].text}</p>
-          `);
-        })
-        .catch((error) => {
-          // Handle error
-          res.send(`
-            <h1>Error</h1>
-            <p>${error.message}</p>
-          `);
-        });
-    });
-  });
-});
+    // Parse PDF file
+    pdf(req.files.pdf.data).then((data) => {
+        // Use OpenAI's GPT-3 to generate summary
+        openai.createCompletion({
+            model: "text-davinci-002",
+            prompt: data.text.toString(),
+            temperature: 0.5,
+            max_tokens: parseInt(data.text.toString().length),
+          })
+          .then((response) => {
+            // Display summary
+            res.send(`
+              <h1>PDF Summary</h1>
+              <p>${response.data.choices[0].text}</p>
+            `);
+          })
+          .catch((error) => {
+            // Handle error
+            res.send(`
+              <h1>Error</h1>
+              <p>${error.message}</p>
+            `);
+          });
+      })
+      .catch((error) => {
+        // Handle error from PDF parsing
+        res.send(`
+          <h1>Error</h1>
+          <p>${error.message}</p>
+        `);
+      });
+  });  
 
 // Start server
 app.listen(3000, () => {
