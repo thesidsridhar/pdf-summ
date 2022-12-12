a73b0ca64aaca4060d5f21f65a4fefcf98ae5afa
Siddarth Sridhar <70396053+thesidsridhar@users.noreply.github.com>
2022-12-11 16:06:25 -0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const fileUpload = require("express-fileupload");
 const pdf = require("pdf-parse");
 const inquirer = require('inquirer');
 const openai = require("openai");
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
 // Set OpenAI API Key
 openai.apiKey = "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";
@@ -22,39 +24,44 @@ app.get("/", (req, res) => {
   `);
 });
 
-app.post("/upload", (req, res) => {
-    // Parse PDF file
-    pdf(req.files.pdf.data).then((data) => {
-        // Use OpenAI's GPT-3 to generate summary
-        openai.createCompletion({
-            model: "text-davinci-002",
-            prompt: data.text.toString(),
-            temperature: 0.5,
-            max_tokens: parseInt(data.text.toString().length),
-          })
-          .then((response) => {
-            // Display summary
-            res.send(`
-              <h1>PDF Summary</h1>
-              <p>${response.data.choices[0].text}</p>
-            `);
-          })
-          .catch((error) => {
-            // Handle error
-            res.send(`
-              <h1>Error</h1>
-              <p>${error.message}</p>
-            `);
-          });
+app.post("/upload", upload.single('pdf'), (req, res) => {
+    // Read the uploaded PDF file
+    pdfExtract.extract(req.file.path, {}, (err, data) => {
+      if (err) {
+        // Handle error from PDF parsing
+        res.send(`
+          <h1>Error</h1>
+          <p>${err.message}</p>
+        `);
+        return;
+      }
+  
+      // Convert the extracted text to a string
+      const text = data.pages.map(page => page.content).join('\n');
+  
+      // Use OpenAI's GPT-3 to generate summary
+      openai.createCompletion({
+        model: "text-davinci-002",
+        prompt: text,
+        temperature: 0.5,
+        max_tokens: text.length,
+      })
+      .then((response) => {
+        // Display summary
+        res.send(`
+          <h1>PDF Summary</h1>
+          <p>${response.data.choices[0].text}</p>
+        `);
       })
       .catch((error) => {
-        // Handle error from PDF parsing
+        // Handle error from OpenAI API
         res.send(`
           <h1>Error</h1>
           <p>${error.message}</p>
         `);
       });
-  });  
+    });
+  });    
 
 // Start server
 app.listen(3000, () => {
